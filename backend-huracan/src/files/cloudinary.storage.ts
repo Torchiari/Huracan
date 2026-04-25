b9fa2e030f47bc5fa41b54f7ab3b704eba9380b5
Danel Torchiari <danel.t@hotmail.com>
2026-04-25 04:06:24 -0300

    prueba pdf

--- backend-huracan/src/files/cloudinary.storage.ts
+++ backend-huracan/src/files/cloudinary.storage.ts
@@ -1,13 +1,16 @@
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import cloudinary from './cloudinary.config';
+import { v2 as cloudinary } from 'cloudinary';
 
 export const storage = new CloudinaryStorage({
   cloudinary,
   params: async (req, file) => {
+    const isPdf = file.mimetype === 'application/pdf';
+
     return {
       folder: 'huracan',
-      resource_type: 'auto',
+      resource_type: isPdf ? 'raw' : 'image',
       public_id: Date.now() + '-' + file.originalname.split('.')[0],
+      format: isPdf ? 'pdf' : undefined,
     };
   },
 });
